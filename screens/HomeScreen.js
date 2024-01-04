ad2fb3ca94e697777d3818566a67071d827782b1
Ana <aninha10.desouza@gmail.com>
2024-01-04 10:19:00 -0300

    Buttom com navigation criado

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HeroImage } from "../assets";
+import * as Animatable from "react-native-animatable";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -35,15 +36,29 @@ const HomeScreen = () => {
         </Text>
       </View>
 
-
       <View className="w-[400px] h-[400px] bg-[#BD55AF] rounded-full absolute bottom-20 -right-40"></View>
       <View className="w-[400px] h-[400px] bg-[#00F088] rounded-full absolute -bottom-28 -left-60"></View>
 
-      <View className="flex-1 relative items-baseline justify-center">
-        <Image
-        source={HeroImage}
-        className="w-full h-full object-cover left-10 mt-22"
+      <View className="flex-1 relative items-center justify-center">
+        <Animatable.Image
+          animation="fadeIn"
+          easing="ease-in-out"
+          iterationCount="infinite"
+          source={HeroImage}
+          className="w-full h-full object-cover left-10 mt-22"
         />
+        <TouchableOpacity 
+        onPress={() => navigation.navigate("Discover")}
+        className="absolute bottom-39 w-24 h-24 border-l-2 border-r-2  border-t-4 border-[#BD55AF] rounded-full items-center justify-center">
+          <Animatable.View
+            animation={"pulse"}
+            easing="ease-in-out"
+            iterationCount={"infinite"}
+            className="w-20 h-20 items-center justify-center rounded-full bg-[#BD55AF]"
+          >
+            <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
+          </Animatable.View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
